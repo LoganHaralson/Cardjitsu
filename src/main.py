a7a70a1e517e2ec71439130ecb63619ce5c1edec
LoganHaralson <haralsonlogan9@gmail.com>
2023-03-25 17:11:18 -0400

    deleted main and changed main2 to main

--- src/main.py
+++ src/main.py
@@ -1,113 +1,170 @@
 import random
-
-# Define the choices and ranks
-elements = ['Fire', 'Water', 'Snow']
-ranks = ['1', '2', '3', '4', '5', '6']
-# Define which choices win against the other
-strengths = {
-    'Fire': 'Snow',
-    'Snow': 'Water',
-    'Water': 'Fire'
-}
-
-weaknesses = {
-    'Fire': 'Water',
-    'Snow': 'Fire',
-    'Water': 'Snow'
-}
-
-# Define wins
-player_wins = 0
-computer_wins = 0
-
-# Create a deck using the choices above
-original_deck = [(rank, element) for rank in ranks for element in elements]
-
-# Copy the deck for use in game
-deck = original_deck.copy()
-
-# Draw a hand of three cards for the player
-hand = random.sample(deck, 3)
-
-# Draw a hidden hand of three cards for the computer
-hidden_hand = random.sample(deck, 3)
-
-# Define compare cards function
+import unittest
 
 
-def compare_cards(player_card, computer_card):
-    global player_wins
-    global computer_wins
-    if player_card:
-        if strengths[player_card[1]] == computer_card[1]:
-            print('You won this round')
-            player_wins += 1
-            print('{} : {}'.format(player_wins, computer_wins))
-        elif weaknesses[player_card[1]] == computer_card[1]:
-            print('The computer won this round')
-            computer_wins += 1
-            print('{} : {}'.format(player_wins, computer_wins))
+class Element:
+    weaknesses = {
+        'Fire': 'Water',
+        'Snow': 'Fire',
+        'Water': 'Snow'
+    }
+
+    def __init__(self, element):
+        self.element = element
+
+    def __lt__(self, other):
+        return self.weaknesses[self.element] == other.element
+
+    def __eq__(self, other):
+        return self.element == other.element
+
+    def __str__(self):
+        return self.element
+
+
+class Card:
+    def __init__(self, color, rank, element):
+        self.color = color
+        self.rank = rank
+        self.element = Element(element)
+
+    def __str__(self):
+        return f"{self.color} {self.rank} of {self.element}"
+
+    def __lt__(self, other):
+        if self.element < other.element:
+            return True
+        elif self.element == other.element:
+            return self.rank < other.rank
         else:
-            if player_card[0] > computer_card[0]:
-                print('You won this round.')
-                player_wins += 1
-                print('{} : {}'.format(player_wins, computer_wins))
-            elif player_card[0] < computer_card[0]:
-                print('The computer won this round')
-                computer_wins += 1
-                print('{} : {}'.format(player_wins, computer_wins))
-            else:
-                print("This shouldn't be possible")
+            return False
 
 
-while True:
-    # print the player's hand
-    if len(hand) > 0:
-        print('your hand')
-        for i, card in enumerate(hand):
-            print('[{}]:{} of {}'.format(i+1, card[0], card[1]))
+class GameState:
+    def __init__(self):
+        self.player1 = Player()
+        self.player2 = Player()
 
-        # Prompt the user to play a choice
-        while True:
+    # for each round--
+    def play_round(self):
 
-            # Determine if there are any cards in hand
-            if len(hand) > 0:
-                player_choice = input(
-                    'Awaiting input. Please select an option from above: ')
-                if player_choice.isdigit() and 1 <= int(player_choice) <= len(hand):
-                    break
-                elif player_choice.isdigit() and int(player_choice) > len(hand):
-                    print('Invalid input. Please try again.')
-
-        # Play the selected card and remove it from your hand and the deck
-        if len(hand) == 0:
-            break
-        index = int(player_choice) - 1
-        player_choice, player_rank = hand.pop(index)
-        player_card = (player_choice, player_rank)
-        print('You played the {} of {}.'.format(player_choice, player_rank))
-
-        # Play a random card from the hidden hand
-        computer_choice, computer_rank = random.choice(hidden_hand)
-        computer_card = (computer_choice, computer_rank)
-        hidden_hand.remove(computer_card)
-        print('The computer played the {} of {}'.format(
-            computer_choice, computer_rank))
-
-        # Determine and print the winner of the round
-        compare_cards(player_card, computer_card)
-
-        # Draw a new card to each hand
-        if len(deck) > 0:
-            hand.append(deck.pop())
-            hidden_hand.append(deck.pop())
+        self.player1.draw_card()
+        self.player2.draw_card()
+        # both players play a card
+        card1 = self.player1.choose_card()
+        card2 = self.player2.choose_card()
+        print(f"{card1}, {card2}")
+        # compare the cards and collect the winning card
+        if card1 < card2:
+            self.player2.collect_card(card2)
+            print("Player 2 collected their card.")
+        elif card2 < card1:
+            self.player1.collect_card(card1)
+            print("Player 1 collected their card.")
         else:
-            print('There are no cards left to draw')
-            if len(hand) == 0:
-                if player_wins > computer_wins:
-                    print('You won this match')
-                elif computer_wins > player_wins:
-                    print('The computer won this match')
-                else:
-                    print('This match ended in a draw')
-                break
+            print("No cards were collected.")
+
+        # determine a winning player based on collected cards
+        # a player wins by collecting three cards of different colors with the same element or all three elements
+
+
+class Player:
+    def __init__(self):
+        self.deck = Deck()
+        self.deck.shuffle()
+        self.hand = self.deck.draw_hand()
+        self.card_collection = []
+
+    def draw_card(self):
+        self.hand.append(self.deck.take_card())
+
+    def choose_card(self):
+        return self.hand.pop()
+
+    def collect_card(self, card):
+        self.card_collection.append(card)
+
+
+class Deck:
+    colors = ['red', 'blue', 'orange', 'green', 'yellow', 'purple']
+    ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    elements = ['Fire', 'Water', 'Snow']
+# creates a collection of cards
+
+    def __init__(self):
+        cards = []
+        for color in self.colors:
+            for rank in self.ranks:
+                for element in self.elements:
+                    card = Card(color, rank, element)
+                    cards.append(card)
+        self.cards = cards
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def take_card(self):
+        return self.cards.pop(0)
+
+    def draw_hand(self):
+        hand = []
+        for i in range(4):
+            hand.append(self.take_card())
+        return hand
+
+
+class TestFunctions(unittest.TestCase):
+    def test_element_eq(self):
+        fire = Element("Fire")
+        water = Element("Water")
+        snow = Element("Snow")
+        self.assertTrue(fire == fire)
+        self.assertTrue(water == water)
+        self.assertTrue(snow == snow)
+        self.assertFalse(snow == fire)
+        self.assertFalse(fire == water)
+        self.assertFalse(water == snow)
+
+    def test_element_lt(self):
+        fire = Element("Fire")
+        water = Element("Water")
+        snow = Element("Snow")
+        self.assertTrue(fire < water)
+        self.assertTrue(water < snow)
+        self.assertTrue(snow < fire)
+        self.assertFalse(snow < snow)
+        self.assertFalse(fire < fire)
+        self.assertFalse(water < water)
+        self.assertFalse(fire < snow)
+        self.assertFalse(water < fire)
+        self.assertFalse(snow < water)
+
+    def test_card_eq(self):
+        card1 = Card('orange', 4, 'Fire')
+        card2 = Card('purple', 7, 'Water')
+        card3 = Card('blue', 5, 'Snow')
+        self.assertTrue(card1 == card1)
+        self.assertTrue(card2 == card2)
+        self.assertTrue(card3 == card3)
+        self.assertFalse(card1 == card2)
+        self.assertFalse(card2 == card3)
+        self.assertFalse(card3 == card1)
+
+    def test_card_lt(self):
+        card1 = Card('orange', 4, 'Fire')
+        card2 = Card('purple', 7, 'Water')
+        card3 = Card('blue', 5, 'Water')
+        self.assertTrue(card1 < card2)
+        self.assertTrue(card1 < card3)
+        self.assertTrue(card3 < card2)
+        self.assertFalse(card2 < card1)
+        self.assertFalse(card2 < card3)
+        self.assertFalse(card3 < card1)
+
+
+if __name__ == "__main__":
+    # unittest.main()
+    gamestate = GameState()
+    while True:
+        gamestate.play_round()
+        # find a way to get user input and pass it to the gamestate
